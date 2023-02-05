653eb048e32c051d90d0326e137e485059722d0a
Charles Carlson <c.bayardcarlson@gmail.com>
2023-02-05 13:08:38 -0800

    adding modules for data import

---
+++ data/modules/dataImporter.py
@@ -0,0 +1,22 @@
+"""This module defines functions to import the data."""
+
+from modules.constants import YELP_DATASETS
+import pandas as pd
+import numpy as np
+
+def yelp_import(size="large"):
+    """This returns a dictionary of DFs with the Yelp Data.
+
+    Args:
+        size - str: A string denoting if the dataset import should be the full dataset or a smaller dataset.
+    """
+
+    if size == "small":
+        path_start = "yelp_dataset/smaller/"
+    else:
+        path_start = "yelp_dataset/"
+
+    end_dict = {}
+    for name, file_name in YELP_DATASETS.items():
+        end_dict[name] = pd.read_json(path_start + file_name, lines = True)
+    return end_dict
