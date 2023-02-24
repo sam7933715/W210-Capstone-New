d39f79074e25dfe118d593c1bcf0abd3d5267779
Charles Carlson <charlescarlson@Bayards-Macbook-Pro.local>
2023-02-24 06:48:27 -0800

    finishing venv

---
+++ trip-planner/modules/spark_nlp.py
@@ -0,0 +1,11 @@
+"""This is a module to train on the yelp dataset to recommend trips."""
+
+import pyspark
+import spark_nlp
+
+
+class TripModel:
+    """This is a work in progress module that will be used to train a Spark NLP model."""
+
+    def __init__(self):
+        self.WIP = True
