f78607a05b6895b15b516d36fedbe7bc790e305e
ArrianaMD <arriana.daniels@gmail.com>
2023-03-05 10:06:47 -0800

    Updating name of module to match expected module name

--- trip-planner/modules/pretrainedModels.py
+++ trip-planner/modules/pretrainedModels.py
@@ -1,16 +1,23 @@
 """This module holds a class to import models and add methods that the app needs"""
 
-import random as rand
+import pickle
 
-class pretrainedModel():
-    """Takes in a pretrained model and adds methods for use by the app."""
 
-    def __init__(self, model):
-        self.model = model
+def save_model(filename, modelObject):
+    """ filename : string containing target filename
+        modelObject : model object """
+    f = filename
+    ml = modelObject
+
+    with open(f, 'wb') as f:
+        pickle.dump(ml, f)
+
+
+def load_model(filename):
+    """ pass in a file name. Output will be pickle file containing model
+        You can then predict as usual"""
+    with open(filename, 'rb') as f:
+        ml = pickle.load(f)
+
+    return ml
     
-    def similarity(self, X, Y):
-        """A helper function to list how similar two establishments are."""
-
-        # This doesn't currently have useful functionality. 
-        # This needs to be updated to determine how similar two points are using cosine similarity. 
-        return rand.random()
